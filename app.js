35a703cb8e2f726625a4f6724f5f5c7beb9529c5
Ayush Rathore <131510947+ayushrathore1@users.noreply.github.com>
2025-10-30 21:17:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.use(cors({
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const morgan = require("morgan");
 const path = require("path");
 
