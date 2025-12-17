d225275f0c15666d344d26af2da068a469f7af82
ayush rathore <rathoreayush512@gmail.com>
2025-12-17 19:19:12 +0530

    increase the file upload size limit to 50 mb

--- middleware/upload.js
+++ middleware/upload.js
@@ -56,7 +56,7 @@ const storage = new CloudinaryStorage({
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit
+  limits: { fileSize: 50 * 1024 * 1024 }, // 50 MB limit
   fileFilter: fileFilter,
 });
 
